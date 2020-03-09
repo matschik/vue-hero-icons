da3ccf8e207ec83869f68be41e2a1b45287eb5a8
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2020-03-09 22:07:18 +0100

    docs: remove unused css

--- docs/index.js
+++ docs/index.js
@@ -5,16 +5,6 @@ import Vue from 'vue'
 import './main.css'
 import App from './App.vue'
 
-// Vue.use(Tippy, {
-//   position: 'right',
-//   theme: 'light',
-//   size: 'small',
-//   animation: 'perspective',
-//   performance: true,
-//   arrow: true,
-//   trigger: 'click'
-// })
-
 new Vue({
   el: '#app',
   render: h => h(App)
