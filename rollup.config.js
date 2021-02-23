96f9428f372a35cde465cb2cab348a6187447fe6
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2021-02-23 17:18:03 +0100

    fix build and heroicons as dev dependency

--- rollup.config.js
+++ rollup.config.js
@@ -1,61 +1,55 @@
-import babel from "rollup-plugin-babel";
-import {pascalCase} from "pascal-case";
+import { babel } from "@rollup/plugin-babel";
+import { pascalCase } from "pascal-case";
 
-const plugins = [
-  babel({
-    presets: ["@vue/babel-preset-jsx"],
-    exclude: "node_modules/**"
-  })
-];
+const plugins = [babel({ babelHelpers: "bundled" })];
 
 const builds = [
   {
     name: "outline",
     input: "./packages/outline/index.js",
-    outputDir: `./packages/outline/lib`
+    outputDir: `./packages/outline/lib`,
   },
   {
     name: "solid",
     input: "./packages/solid/index.js",
-    outputDir: `./packages/solid/lib`
-  }
-]
-  .map(({ name, input, outputDir }) => {
-    return [
-      /* esm */
-      {
-        input,
-        plugins,
-        output: {
-          file: `${outputDir}/index.es.js`,
-          format: "es",
-          sourcemap: true
-        }
+    outputDir: `./packages/solid/lib`,
+  },
+].map(({ name, input, outputDir }) => {
+  return [
+    /* esm */
+    {
+      input,
+      plugins,
+      output: {
+        file: `${outputDir}/index.es.js`,
+        format: "es",
+        sourcemap: true,
       },
+    },
 
-      /* cjs */
-      {
-        input,
-        plugins,
-        output: {
-          file: `${outputDir}/index.cjs.js`,
-          format: "cjs",
-          sourcemap: true
-        }
+    /* cjs */
+    {
+      input,
+      plugins,
+      output: {
+        file: `${outputDir}/index.cjs.js`,
+        format: "cjs",
+        sourcemap: true,
       },
+    },
 
-      /* umd */
-      {
-        input,
-        plugins,
-        output: {
-          file: `${outputDir}/index.umd.js`,
-          format: "umd",
-          name: `vueHeroIcons${pascalCase(name)}`,
-          sourcemap: true
-        }
-      }
-    ];
-  });
+    /* umd */
+    {
+      input,
+      plugins,
+      output: {
+        file: `${outputDir}/index.umd.js`,
+        format: "umd",
+        name: `vueHeroIcons${pascalCase(name)}`,
+        sourcemap: true,
+      },
+    },
+  ];
+});
 
 export default [].concat(...builds);
